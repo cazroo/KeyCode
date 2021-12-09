3b3c04842469421b202f8cff73c0782c1f38ae1d
Caroline Rooney <caroline_rooney@icloud.com>
2021-12-09 16:20:39 +0000

    keycode

---
+++ app.js
@@ -0,0 +1,15 @@
+const button = document.getElementById("button");
+const input = document.getElementById("input");
+const header = document.getElementById("header");
+const intro = document.getElementById("intro");
+const main = document.getElementById("body");
+
+document.addEventListener("keydown", (event) => {
+    header.textContent=event.which
+    key.textContent=event.key
+    code.textContent=event.code
+    which.textContent=event.which
+
+    intro.style.display = "none"
+    main.style.display = "block"
+});
